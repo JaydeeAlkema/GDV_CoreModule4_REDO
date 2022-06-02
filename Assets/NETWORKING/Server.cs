feea08b27c28dbee0cf00247529f752896c1dbab
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2022-06-02 12:23:05 +0200

    Begin adding fire bullet behaviour.

--- Assets/NETWORKING/Server.cs
+++ Assets/NETWORKING/Server.cs
@@ -5,7 +5,8 @@ using Unity.Networking.Transport;
 using Unity.Collections;
 using Unity.Networking.Transport.Utilities;
 
-namespace ChatClientExample {
+namespace ChatClientExample
+{
 
     public delegate void ServerMessageHandler(Server server, NetworkConnection con, MessageHeader header);
     public delegate void ClientMessageHandler(Client client, MessageHeader header);
@@ -25,21 +26,21 @@ namespace ChatClientExample {
     }
 
     public enum MessageType
-	{
-        MESSAGE, 
+    {
+        MESSAGE,
         JOIN,
         QUIT
-	}
+    }
 
     public class PingPong
-	{
+    {
         public float lastSendTime = 0;
         public int status = -1;
         public string name = ""; // because of weird issues...
-	}
+    }
 
     public static class NetworkMessageInfo
-	{
+    {
         public static Dictionary<NetworkMessageType, System.Type> TypeMap = new Dictionary<NetworkMessageType, System.Type> {
             { NetworkMessageType.HANDSHAKE,                 typeof(HandshakeMessage) },
             { NetworkMessageType.HANDSHAKE_RESPONSE,        typeof(HandshakeResponseMessage) },
@@ -75,7 +76,8 @@ namespace ChatClientExample {
         public ChatCanvas chat;
         public NetworkManager networkManager;
 
-        void Start() {
+        void Start()
+        {
             // Create Driver
             m_Driver = NetworkDriver.Create(new ReliableUtility.Parameters { WindowSize = 32 });
             m_Pipeline = m_Driver.CreatePipeline(typeof(ReliableSequencedPipelineStage));
@@ -93,18 +95,22 @@ namespace ChatClientExample {
 
         // Write this immediately after creating the above Start calls, so you don't forget
         //  Or else you well get lingering thread sockets, and will have trouble starting new ones!
-        void OnDestroy() {
+        void OnDestroy()
+        {
             m_Driver.Dispose();
             m_Connections.Dispose();
         }
 
-        void Update() {
+        void Update()
+        {
             // This is a jobified system, so we need to tell it to handle all its outstanding tasks first
             m_Driver.ScheduleUpdate().Complete();
 
             // Clean up connections, remove stale ones
-            for (int i = 0; i < m_Connections.Length; i++) {
-                if (!m_Connections[i].IsCreated) {
+            for (int i = 0; i < m_Connections.Length; i++)
+            {
+                if (!m_Connections[i].IsCreated)
+                {
                     m_Connections.RemoveAtSwapBack(i);
                     // This little trick means we can alter the contents of the list without breaking/skipping instances
                     --i;
@@ -113,20 +119,24 @@ namespace ChatClientExample {
 
             // Accept new connections
             NetworkConnection c;
-            while ((c = m_Driver.Accept()) != default(NetworkConnection)) {
+            while ((c = m_Driver.Accept()) != default(NetworkConnection))
+            {
                 m_Connections.Add(c);
                 // Debug.Log("Accepted a connection");
             }
 
             DataStreamReader stream;
-            for (int i = 0; i < m_Connections.Length; i++) {
+            for (int i = 0; i < m_Connections.Length; i++)
+            {
                 if (!m_Connections[i].IsCreated)
                     continue;
 
                 // Loop through available events
                 NetworkEvent.Type cmd;
-                while ((cmd = m_Driver.PopEventForConnection(m_Connections[i], out stream)) != NetworkEvent.Type.Empty) {
-                    if (cmd == NetworkEvent.Type.Data) {
+                while ((cmd = m_Driver.PopEventForConnection(m_Connections[i], out stream)) != NetworkEvent.Type.Empty)
+                {
+                    if (cmd == NetworkEvent.Type.Data)
+                    {
                         // First UInt is always message type (this is our own first design choice)
                         NetworkMessageType msgType = (NetworkMessageType)stream.ReadUShort();
 
@@ -134,15 +144,19 @@ namespace ChatClientExample {
                         MessageHeader header = (MessageHeader)System.Activator.CreateInstance(NetworkMessageInfo.TypeMap[msgType]);
                         header.DeserializeObject(ref stream);
 
-                        if (networkMessageHandlers.ContainsKey(msgType)) {
-                            try {
+                        if (networkMessageHandlers.ContainsKey(msgType))
+                        {
+                            try
+                            {
                                 networkMessageHandlers[msgType].Invoke(this, m_Connections[i], header);
                             }
-                            catch {
+                            catch
+                            {
                                 Debug.LogError($"Badly formatted message received: {msgType}");
                             }
                         }
-                        else {
+                        else
+                        {
                             Debug.LogWarning($"Unsupported message type received: {msgType}", this);
                         }
                     }
@@ -150,18 +164,23 @@ namespace ChatClientExample {
             }
 
             // Ping Pong stuff for timeout disconnects
-            for (int i = 0; i < m_Connections.Length; i++) {
+            for (int i = 0; i < m_Connections.Length; i++)
+            {
                 if (!m_Connections[i].IsCreated)
                     continue;
-                
-                if ( pongDict.ContainsKey(m_Connections[i])) {
-                    if ( Time.time - pongDict[m_Connections[i]].lastSendTime > 5f ) {
+
+                if (pongDict.ContainsKey(m_Connections[i]))
+                {
+                    if (Time.time - pongDict[m_Connections[i]].lastSendTime > 5f)
+                    {
                         pongDict[m_Connections[i]].lastSendTime = Time.time;
-                        if (pongDict[m_Connections[i]].status == 0 ) {
+                        if (pongDict[m_Connections[i]].status == 0)
+                        {
                             // Remove from all the dicts, save name / id for msg
 
                             // FIXME: for some reason, sometimes this isn't in the list?
-                            if (nameList.ContainsKey(m_Connections[i])) {
+                            if (nameList.ContainsKey(m_Connections[i]))
+                            {
                                 nameList.Remove(m_Connections[i]);
                             }
 
@@ -178,13 +197,15 @@ namespace ChatClientExample {
                             // Build messages
                             string msg = $"{name} has been Disconnected (connection timed out)";
                             chat.NewMessage(msg, ChatCanvas.leaveColor);
-                        
-                            ChatMessage quitMsg = new ChatMessage {
+
+                            ChatMessage quitMsg = new ChatMessage
+                            {
                                 message = msg,
                                 messageType = MessageType.QUIT
                             };
-                            
-                            DestroyMessage destroyMsg = new DestroyMessage {
+
+                            DestroyMessage destroyMsg = new DestroyMessage
+                            {
                                 networkId = destroyId
                             };
 
@@ -195,14 +216,16 @@ namespace ChatClientExample {
                             // Clean up
                             m_Connections[i] = default;
                         }
-                        else {
+                        else
+                        {
                             pongDict[m_Connections[i]].status -= 1;
                             PingMessage pingMsg = new PingMessage();
                             SendUnicast(m_Connections[i], pingMsg);
                         }
                     }
                 }
-                else if ( nameList.ContainsKey(m_Connections[i]) ) { //means they've succesfully handshaked
+                else if (nameList.ContainsKey(m_Connections[i]))
+                { //means they've succesfully handshaked
                     PingPong ping = new PingPong();
                     ping.lastSendTime = Time.time;
                     ping.status = 3;    // 3 retries
@@ -215,23 +238,28 @@ namespace ChatClientExample {
             }
         }
 
-        public void SendUnicast( NetworkConnection connection, MessageHeader header, bool realiable = true ) {
+        public void SendUnicast(NetworkConnection connection, MessageHeader header, bool realiable = true)
+        {
             DataStreamWriter writer;
             int result = m_Driver.BeginSend(realiable ? m_Pipeline : NetworkPipeline.Null, connection, out writer);
-            if (result == 0) {
+            if (result == 0)
+            {
                 header.SerializeObject(ref writer);
                 m_Driver.EndSend(writer);
             }
         }
 
-        public void SendBroadcast(MessageHeader header, NetworkConnection toExclude = default, bool realiable = true) {
-            for (int i = 0; i < m_Connections.Length; i++) {
+        public void SendBroadcast(MessageHeader header, NetworkConnection toExclude = default, bool realiable = true)
+        {
+            for (int i = 0; i < m_Connections.Length; i++)
+            {
                 if (!m_Connections[i].IsCreated || m_Connections[i] == toExclude)
                     continue;
 
                 DataStreamWriter writer;
                 int result = m_Driver.BeginSend(realiable ? m_Pipeline : NetworkPipeline.Null, m_Connections[i], out writer);
-                if (result == 0) {
+                if (result == 0)
+                {
                     header.SerializeObject(ref writer);
                     m_Driver.EndSend(writer);
                 }
@@ -243,8 +271,9 @@ namespace ChatClientExample {
         //  - Client chat message               (DONE)
         //  - Client chat exit                  (DONE)
         //  - Input update
-        
-        static void HandleClientHandshake(Server serv, NetworkConnection connection, MessageHeader header) {
+
+        static void HandleClientHandshake(Server serv, NetworkConnection connection, MessageHeader header)
+        {
             HandshakeMessage message = header as HandshakeMessage;
 
             // Add to list
@@ -252,7 +281,8 @@ namespace ChatClientExample {
             string msg = $"{message.name.ToString()} has joined the chat.";
             serv.chat.NewMessage(msg, ChatCanvas.joinColor);
 
-            ChatMessage chatMsg = new ChatMessage {
+            ChatMessage chatMsg = new ChatMessage
+            {
                 messageType = MessageType.JOIN,
                 message = msg
             };
@@ -263,32 +293,38 @@ namespace ChatClientExample {
             // spawn a non-local, server player
             GameObject player;
             uint networkId = 0;
-            if (serv.networkManager.SpawnWithId(NetworkSpawnObject.PLAYER, NetworkManager.NextNetworkID, out player)) {
+            if (serv.networkManager.SpawnWithId(NetworkSpawnObject.PLAYER, NetworkManager.NextNetworkID, out player))
+            {
                 // Get and setup player instance
                 NetworkedPlayer playerInstance = player.GetComponent<NetworkedPlayer>();
                 playerInstance.isServer = true;
                 playerInstance.isLocal = false;
+                playerInstance.userName = message.name.ToString();
                 networkId = playerInstance.networkId;
 
                 serv.playerInstances.Add(connection, playerInstance);
 
                 // Send spawn local player back to sender
-                HandshakeResponseMessage responseMsg = new HandshakeResponseMessage {
+                HandshakeResponseMessage responseMsg = new HandshakeResponseMessage
+                {
                     message = $"Welcome {message.name.ToString()}!",
                     networkId = playerInstance.networkId
                 };
 
                 serv.SendUnicast(connection, responseMsg);
             }
-            else {
+            else
+            {
                 Debug.LogError("Could not spawn player instance");
             }
 
             // Send all existing players to this player
-            foreach (KeyValuePair<NetworkConnection, NetworkedPlayer> pair in serv.playerInstances) {
+            foreach (KeyValuePair<NetworkConnection, NetworkedPlayer> pair in serv.playerInstances)
+            {
                 if (pair.Key == connection) continue;
 
-                SpawnMessage spawnMsg = new SpawnMessage {
+                SpawnMessage spawnMsg = new SpawnMessage
+                {
                     networkId = pair.Value.networkId,
                     objectType = NetworkSpawnObject.PLAYER
                 };
@@ -297,22 +333,27 @@ namespace ChatClientExample {
             }
 
             // Send creation of this player to all existing players
-            if (networkId != 0) {
-                SpawnMessage spawnMsg = new SpawnMessage {
+            if (networkId != 0)
+            {
+                SpawnMessage spawnMsg = new SpawnMessage
+                {
                     networkId = networkId,
                     objectType = NetworkSpawnObject.PLAYER
                 };
                 serv.SendBroadcast(spawnMsg, connection);
             }
-            else {
+            else
+            {
                 Debug.LogError("Invalid network id for broadcasting creation");
             }
         }
 
-        static void HandleClientMessage(Server serv, NetworkConnection connection, MessageHeader header) {
+        static void HandleClientMessage(Server serv, NetworkConnection connection, MessageHeader header)
+        {
             ChatMessage receivedMsg = header as ChatMessage;
 
-            if (serv.nameList.ContainsKey(connection)) {
+            if (serv.nameList.ContainsKey(connection))
+            {
                 string msg = $"{serv.nameList[connection]}: {receivedMsg.message}";
                 serv.chat.NewMessage(msg, ChatCanvas.chatColor);
 
@@ -321,22 +362,26 @@ namespace ChatClientExample {
                 // forward message to all clients
                 serv.SendBroadcast(receivedMsg);
             }
-            else {
+            else
+            {
                 Debug.LogError($"Received message from unlisted connection: {receivedMsg.message}");
             }
         }
 
-        static void HandleClientExit(Server serv, NetworkConnection connection, MessageHeader header) {
+        static void HandleClientExit(Server serv, NetworkConnection connection, MessageHeader header)
+        {
             ChatQuitMessage quitMsg = header as ChatQuitMessage;
 
-            if (serv.nameList.ContainsKey(connection)) {
+            if (serv.nameList.ContainsKey(connection))
+            {
                 string msg = $"{serv.nameList[connection]} has left the chat.";
                 serv.chat.NewMessage(msg, ChatCanvas.leaveColor);
 
                 // Clean up
                 serv.nameList.Remove(connection);
                 // if you join and quit quickly, might not be in this dict yet
-                if (serv.pongDict.ContainsKey(connection)) {
+                if (serv.pongDict.ContainsKey(connection))
+                {
                     serv.pongDict.Remove(connection);
                 }
 
@@ -347,12 +392,14 @@ namespace ChatClientExample {
                 serv.playerInstances.Remove(connection);
 
                 // Build messages
-                ChatMessage chatMsg = new ChatMessage {
+                ChatMessage chatMsg = new ChatMessage
+                {
                     message = msg,
                     messageType = MessageType.QUIT
                 };
 
-                DestroyMessage destroyMsg = new DestroyMessage {
+                DestroyMessage destroyMsg = new DestroyMessage
+                {
                     networkId = destroyId
                 };
 
@@ -360,28 +407,35 @@ namespace ChatClientExample {
                 serv.SendBroadcast(chatMsg, connection);
                 serv.SendBroadcast(destroyMsg, connection);
             }
-            else {
+            else
+            {
                 Debug.LogError("Received exit from unlisted connection");
             }
         }
 
-        static void HandleClientInput(Server serv, NetworkConnection connection, MessageHeader header) {
+        static void HandleClientInput(Server serv, NetworkConnection connection, MessageHeader header)
+        {
             InputUpdateMessage inputMsg = header as InputUpdateMessage;
 
-            if (serv.playerInstances.ContainsKey(connection)) {
-                if (serv.playerInstances[connection].networkId == inputMsg.networkId) {
+            if (serv.playerInstances.ContainsKey(connection))
+            {
+                if (serv.playerInstances[connection].networkId == inputMsg.networkId)
+                {
                     serv.playerInstances[connection].UpdateInput(inputMsg.input);
                 }
-                else {
+                else
+                {
                     Debug.LogError("NetworkID Mismatch for Player Input");
                 }
             }
-            else {
+            else
+            {
                 Debug.LogError("Received player input from unlisted connection");
             }
         }
 
-        static void HandleClientPong(Server serv, NetworkConnection connection, MessageHeader header) {
+        static void HandleClientPong(Server serv, NetworkConnection connection, MessageHeader header)
+        {
             // Debug.Log("PONG");
             serv.pongDict[connection].status = 3;   //reset retry count
         }
